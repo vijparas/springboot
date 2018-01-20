48d4fef9ed38a2b858e19b3bb4e799dacf6b4c7c
Paras <vijparas@gmail.com>
2018-01-20 15:05:24 +0530

    Integrated thymeleaf and displayed list of books with publisher in thymeleaf template

--- src/main/java/in/paras/springwebapp/model/Publisher.java
+++ src/main/java/in/paras/springwebapp/model/Publisher.java
@@ -10,6 +10,9 @@ public class Publisher {
     private String name;
     private String address;
 
+    public Publisher(){
+
+    }
     public Publisher(String name,String address){
         this.name=name;
         this.address=address;
